164ab8950717faf0ba9704855d847cb23ec899de
Harry <shangdal@ghc31.ghc.andrew.cmu.edu>
2017-11-27 16:24:48 -0500

    Init Code

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+  cout << "Hello World!" << endl;
+
+  return 0;
+}
